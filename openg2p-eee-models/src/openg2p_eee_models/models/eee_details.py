9bfaada72a244a722504f7dedde1a14368dd2087
tahzeer <tahzeer.ash@gmail.com>
2025-04-09 17:19:38 +0530

    Edit disbursement models and update registrant_id to string (changed from id to unique_id)

--- openg2p-eee-models/src/openg2p_eee_models/models/eee_details.py
+++ openg2p-eee-models/src/openg2p_eee_models/models/eee_details.py
@@ -1,5 +1,5 @@
 from openg2p_fastapi_common.models import BaseORMModel
-from sqlalchemy import Float, Integer, String
+from sqlalchemy import Float, String
 from sqlalchemy.orm import mapped_column
 
 
@@ -7,5 +7,5 @@ class EEEDetails(BaseORMModel):
     __tablename__ = "eee_details"
 
     pbms_request_id = mapped_column(String, primary_key=True)
-    registrant_id = mapped_column(Integer, primary_key=True)
+    registrant_id = mapped_column(String, primary_key=True)
     quantity = mapped_column(Float, nullable=False, default=0.0)
