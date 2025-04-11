f2df7e1c7499644a53bf8ffadc0c4a9c93b5b9eb
tahzeer <tahzeer.ash@gmail.com>
2025-04-11 19:52:11 +0530

    Update eligibility beat producer and add entitlement beat producer

--- openg2p-eee-celery-beat-producers/src/openg2p_eee_celery_beat_producers/tasks/eee_eligibility_beat_producer.py
+++ openg2p-eee-celery-beat-producers/src/openg2p_eee_celery_beat_producers/tasks/eee_eligibility_beat_producer.py
@@ -14,9 +14,9 @@ _logger = logging.getLogger(_config.logging_default_logger_name)
 _engine = get_engine()
 
 
-@celery_app.task(name="eee_request_beat_producer")
-def eee_request_beat_producer():
-    _logger.info("Checking for pending EEE requests")
+@celery_app.task(name="eee_eligibility_beat_producer")
+def eee_eligibility_beat_producer():
+    _logger.info("Checking for pending EEE eligibility requests")
     pbms_session_maker = sessionmaker(
         bind=_engine.get("db_engine_pbms"), expire_on_commit=False
     )
@@ -30,10 +30,6 @@ def eee_request_beat_producer():
                         G2PQueEEERequest.eligibility_process_status
                         == StatusEnum.PENDING.value
                     )
-                    | (
-                        G2PQueEEERequest.entitlement_process_status
-                        == StatusEnum.PENDING.value
-                    )
                 )
                 .limit(_config.batch_size)
             )
@@ -41,33 +37,28 @@ def eee_request_beat_producer():
             .all()
         )
         _logger.info(
-            f"Found {len(que_eee_requests)} pending requests in {StatusEnum.PENDING.value}"
+            f"Found {len(que_eee_requests)} pending eligibility requests"
         )
 
         for que_eee_request in que_eee_requests:
-            _logger.info(f"Queueing EEE request ID: {que_eee_request.id}")
+            _logger.info(f"Queueing EEE eligibility request ID: {que_eee_request.id}")
 
-            if que_eee_request.eligibility_process_status == StatusEnum.PENDING.value:
-                worker_type = WorkerTypes.ELIGIBILITY_REQUEST_WORKER
-                que_eee_request.eligibility_process_status = StatusEnum.PROCESSING.value
-
-            elif que_eee_request.entitlement_process_status == StatusEnum.PENDING.value:
-                worker_type = WorkerTypes.ENTITLEMENT_REQUEST_WORKER
-                que_eee_request.entitlement_process_status = StatusEnum.PROCESSING.value
+            que_eee_request.eligibility_process_status = StatusEnum.PROCESSING.value
+            pbms_session.add(que_eee_request)
 
             _logger.info(
-                f"Updating status for {worker_type} to PROCESSING in EEE request ID: {que_eee_request.id}"
+                f"Updating status for {WorkerTypes.ELIGIBILITY_REQUEST_WORKER} to PROCESSING in EEE eligibility request ID: {que_eee_request.id}"
             )
-            pbms_session.commit()
 
             # Send task to appropriate celery worker
             celery_app.send_task(
-                worker_type,
+                WorkerTypes.ELIGIBILITY_REQUEST_WORKER,
                 args=(que_eee_request.id,),
                 queue=_config.eee_task_worker_queue,
             )
             _logger.info(
-                f"Sent task to {worker_type} for EEE request ID: {que_eee_request.id}"
+                f"Sent task to {WorkerTypes.ELIGIBILITY_REQUEST_WORKER} for EEE eligibility request ID: {que_eee_request.id}"
             )
+        pbms_session.commit()
 
-    _logger.info("Completed processing pending EEE requests")
+    _logger.info("Completed processing pending EEE eligibility requests")
