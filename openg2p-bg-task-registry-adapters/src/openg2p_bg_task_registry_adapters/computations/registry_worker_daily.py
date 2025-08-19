4825e7071ca91ae344bafcaded28c2cdd569559c
tahzeer <tahzeer.ash@gmail.com>
2025-08-19 22:16:48 +0530

    fix: implement interface methods

--- openg2p-bg-task-registry-adapters/src/openg2p_bg_task_registry_adapters/computations/registry_worker_daily.py
+++ openg2p-bg-task-registry-adapters/src/openg2p_bg_task_registry_adapters/computations/registry_worker_daily.py
@@ -4,6 +4,60 @@ from ..interface import RegistryInterface
 
 
 class RegistryWorkerDaily(RegistryInterface):
+    
+    async def get_summary(
+        self, beneficiary_list_id: str, bg_task_session: Session, formated: bool = False
+    ):
+        return await super().get_summary(beneficiary_list_id, bg_task_session, formated)
+
+    def get_summary_sync(
+        self, beneficiary_list_id: str, bg_task_session: Session
+    ):
+        return super().get_summary_sync(beneficiary_list_id, bg_task_session)
+
+    def compute_eligibility_statistics(
+        self,
+        beneficiary_list_details,
+        base_summary,
+        sr_session,
+        bg_task_session,
+    ):
+        return super().compute_eligibility_statistics(
+            beneficiary_list_details, base_summary, sr_session, bg_task_session
+        )
+
+    def compute_entitlement_statistics(
+        self, beneficiary_list_id: str, bg_task_session: Session, sr_session: Session
+    ):
+        return super().compute_entitlement_statistics(
+            beneficiary_list_id, bg_task_session, sr_session
+        )
+
+    def get_registrants_by_ids(self, registrant_ids):
+        return super().get_registrants_by_ids(registrant_ids)
+
+    async def search_beneficiaries(
+        self,
+        bg_task_session,
+        pbms_session,
+        beneficiary_list_id,
+        target_registry,
+        search_query,
+        page,
+        page_size,
+        order_by,
+    ):
+        return await super().search_beneficiaries(
+            bg_task_session,
+            pbms_session,
+            beneficiary_list_id,
+            target_registry,
+            search_query,
+            page,
+            page_size,
+            order_by,
+        )
+
     # =================================
     # Entitlement Celery Worker Methods
     # =================================
