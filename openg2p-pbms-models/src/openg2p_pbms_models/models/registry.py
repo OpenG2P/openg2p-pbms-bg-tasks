d5e7357c03feacd006d9e3ae637cfe8083be7f41
tahzeer <tahzeer.ash@gmail.com>
2026-02-05 16:33:09 +0530

    style: pre-commit fix

--- openg2p-pbms-models/src/openg2p_pbms_models/models/registry.py
+++ openg2p-pbms-models/src/openg2p_pbms_models/models/registry.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from sqlalchemy import Integer, String, DateTime, Text
+from sqlalchemy import String
 from sqlalchemy.orm import mapped_column
 
 from .base import BaseORMModel
